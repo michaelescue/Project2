e366f0ddae653798c3025c4a315a459c8680e4e9
Michael Escue <mescue@ds.cecs.pdx.edu>
2019-11-01 12:22:37 -0700

    right turn loop

--- Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4.tcl
+++ Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4.tcl
@@ -25,16 +25,16 @@ set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
 set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property webtalk.parent_dir N:/Project2/Project2/project_2.cache/wt [current_project]
-set_property parent.project_path N:/Project2/Project2/project_2.xpr [current_project]
+set_property webtalk.parent_dir N:/ECE540/ECE540_Project2/Project2/Project2/project_2.cache/wt [current_project]
+set_property parent.project_path N:/ECE540/ECE540_Project2/Project2/Project2/project_2.xpr [current_project]
 set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY} [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_repo_paths n:/Project2/proj2_release_r1_0/ece540_ip_repo [current_project]
-set_property ip_output_repo n:/Project2/Project2/project_2.cache/ip [current_project]
+set_property ip_repo_paths n:/ECE540/ECE540_Project2/Project2/proj2_release_r1_0/ece540_ip_repo [current_project]
+set_property ip_output_repo n:/ECE540/ECE540_Project2/Project2/Project2/project_2.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_ip -quiet N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4.xci
-set_property used_in_implementation false [get_files -all n:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_ooc.xdc]
+read_ip -quiet N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4.xci
+set_property used_in_implementation false [get_files -all n:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_ooc.xdc]
 
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -48,7 +48,7 @@ read_xdc dont_touch.xdc
 set_property used_in_implementation false [get_files dont_touch.xdc]
 set_param ips.enableIPCacheLiteLoad 0
 
-set cached_ip [config_ip_cache -export -no_bom -use_project_ipc -dir N:/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1 -new_name blk_mem_gen_4 -ip [get_ips blk_mem_gen_4]]
+set cached_ip [config_ip_cache -export -no_bom -use_project_ipc -dir N:/ECE540/ECE540_Project2/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1 -new_name blk_mem_gen_4 -ip [get_ips blk_mem_gen_4]]
 
 if { $cached_ip eq {} } {
 close [open __synthesis_is_running__ w]
@@ -89,32 +89,32 @@ write_checkpoint -force -noxdef blk_mem_gen_4.dcp
 create_report "blk_mem_gen_4_synth_1_synth_report_utilization_0" "report_utilization -file blk_mem_gen_4_utilization_synth.rpt -pb blk_mem_gen_4_utilization_synth.pb"
 
 if { [catch {
-  file copy -force N:/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4.dcp N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4.dcp
+  file copy -force N:/ECE540/ECE540_Project2/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4.dcp N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  write_verilog -force -mode synth_stub N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.v
+  write_verilog -force -mode synth_stub N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode synth_stub N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.vhdl
+  write_vhdl -force -mode synth_stub N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_verilog -force -mode funcsim N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_sim_netlist.v
+  write_verilog -force -mode funcsim N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode funcsim N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_sim_netlist.vhdl
+  write_vhdl -force -mode funcsim N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -124,47 +124,47 @@ if { [catch {
 
 
 if { [catch {
-  file copy -force N:/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4.dcp N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4.dcp
+  file copy -force N:/ECE540/ECE540_Project2/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4.dcp N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  file rename -force N:/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4_stub.v N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.v
+  file rename -force N:/ECE540/ECE540_Project2/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4_stub.v N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force N:/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4_stub.vhdl N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.vhdl
+  file rename -force N:/ECE540/ECE540_Project2/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4_stub.vhdl N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force N:/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4_sim_netlist.v N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_sim_netlist.v
+  file rename -force N:/ECE540/ECE540_Project2/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4_sim_netlist.v N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force N:/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4_sim_netlist.vhdl N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_sim_netlist.vhdl
+  file rename -force N:/ECE540/ECE540_Project2/Project2/Project2/project_2.runs/blk_mem_gen_4_synth_1/blk_mem_gen_4_sim_netlist.vhdl N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 }; # end if cached_ip 
 
-if {[file isdir N:/Project2/Project2/project_2.ip_user_files/ip/blk_mem_gen_4]} {
+if {[file isdir N:/ECE540/ECE540_Project2/Project2/Project2/project_2.ip_user_files/ip/blk_mem_gen_4]} {
   catch { 
-    file copy -force N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.v N:/Project2/Project2/project_2.ip_user_files/ip/blk_mem_gen_4
+    file copy -force N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.v N:/ECE540/ECE540_Project2/Project2/Project2/project_2.ip_user_files/ip/blk_mem_gen_4
   }
 }
 
-if {[file isdir N:/Project2/Project2/project_2.ip_user_files/ip/blk_mem_gen_4]} {
+if {[file isdir N:/ECE540/ECE540_Project2/Project2/Project2/project_2.ip_user_files/ip/blk_mem_gen_4]} {
   catch { 
-    file copy -force N:/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.vhdl N:/Project2/Project2/project_2.ip_user_files/ip/blk_mem_gen_4
+    file copy -force N:/ECE540/ECE540_Project2/Project2/Project2/project_2.srcs/sources_1/ip/blk_mem_gen_4/blk_mem_gen_4_stub.vhdl N:/ECE540/ECE540_Project2/Project2/Project2/project_2.ip_user_files/ip/blk_mem_gen_4
   }
 }
 file delete __synthesis_is_running__
